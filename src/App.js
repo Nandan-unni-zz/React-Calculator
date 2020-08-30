fb8f0bbf6dc6ad76e991768ab67133e45a5fbaca
nandan-unni <asnqln@gmail.com>
2020-08-30 14:36:42 +0530

    Deployment

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch} from "react-router-dom";
+
 import './App.css';
 import Calculator from './components/Calculator';
 
@@ -6,7 +8,11 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Calculator></Calculator>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" component={Calculator} />
+          </Switch>
+        </BrowserRouter>
       </div>
     );
   }
